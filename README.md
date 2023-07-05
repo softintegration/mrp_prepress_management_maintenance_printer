e9d143b514f88fc18fdd81670487b7c4d56dfa87
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-07-05 21:48:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Link Maintenance printer,Prepress management and MRP Applications
+-----------------------------------------------------------------
+This module makes the link apps: Maintenance printer,MRP and prepress management so that the calculation of duration will be aware about the printer equipements.
+
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Link Maintenance printer,Prepress management and MRP Applications',
+    'author': 'Soft-integration',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Maintenance',
+    'demo': [],
+    'depends': ['mrp_prepress_management','maintenance_printer'],
+    'data': [
+        'views/mrp_workorder_views.xml'
+    ],
+    'license': 'LGPL-3',
+}
